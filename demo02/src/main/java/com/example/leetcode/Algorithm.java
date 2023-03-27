e431669ad205346119f3d0b16269b2fa7271a549
西行寺幽幽子 <1312088938@qq.com>
2023-03-27 17:10:52 +0800

    leetcode

---
+++ demo02/src/main/java/com/example/leetcode/Algorithm.java
@@ -0,0 +1,23 @@
+package com.example.leetcode;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class Algorithm {
+    public static void main(String[] args) {
+//        String string = " Hi,this is first line. This   is second line.";
+//        string=string.replaceAll("[^a-zA-Z]", " ");
+//        System.out.println(string);
+//        String[] str = string.trim().split("[，。？,.? ]");
+//        for (int i = 0; i < str.length; i++) {
+//            System.out.println(str[i]);
+//        }
+//        child cd=new child("mike");
+        int[][] a = {{10}, {23}, {56}, {78}};
+        List<int[]> ints = Arrays.asList(a);
+        a[3][0] = 1;
+
+
+    }
+
+    }
